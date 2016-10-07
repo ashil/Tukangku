54661d2bdae2458ad90315c619ecc1f515ebfc0d
ashil <aziz280895@gmail.com>
2016-10-07 11:34:37 +0700

    Add files via upload

---
+++ tukangku.sql
@@ -0,0 +1,120 @@
+/*
+SQLyog Trial v11.31 (32 bit)
+MySQL - 5.6.25 : Database - tukangku
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tukangku` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tukangku`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `id_customer` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(200) DEFAULT NULL,
+  `tlp` int(11) DEFAULT NULL,
+  `deposito` varchar(100) DEFAULT NULL,
+  `bbm` varchar(9) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `alamat` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id_customer`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `customer` */
+
+/*Table structure for table `jasa` */
+
+DROP TABLE IF EXISTS `jasa`;
+
+CREATE TABLE `jasa` (
+  `id_jasa` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(200) DEFAULT NULL,
+  `tlp` int(11) DEFAULT NULL,
+  `bbm` varchar(9) DEFAULT NULL,
+  `alamat` varchar(500) DEFAULT NULL,
+  `pengalaman` varchar(1000) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_jasa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `jasa` */
+
+/*Table structure for table `login_customer` */
+
+DROP TABLE IF EXISTS `login_customer`;
+
+CREATE TABLE `login_customer` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `terakhir_login` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login_customer` */
+
+/*Table structure for table `login_jasa` */
+
+DROP TABLE IF EXISTS `login_jasa`;
+
+CREATE TABLE `login_jasa` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `terakhir_login` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login_jasa` */
+
+/*Table structure for table `order` */
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+  `id_order` int(11) NOT NULL AUTO_INCREMENT,
+  `harga` varchar(100) NOT NULL,
+  `tgl` date NOT NULL,
+  `jasa` varchar(100) NOT NULL,
+  `saran` varchar(1000) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_order`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `order` */
+
+/*Table structure for table `rating` */
+
+DROP TABLE IF EXISTS `rating`;
+
+CREATE TABLE `rating` (
+  `id_rating` int(11) NOT NULL AUTO_INCREMENT,
+  `rating` int(11) DEFAULT NULL,
+  `id_jasa` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_rating`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `rating` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
